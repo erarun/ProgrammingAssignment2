e8648ffc562f20c78c9b90ddf918a9b4b2e6cb97
Arun Parajuli <er.arun@gmail.com>
2016-02-27 21:20:44 +0545

    solution completed

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,37 @@
 ## benefit to caching the inverse of a matrix rather than computing it repeatedly 
 ## These functions demonstrate the way the inverse of a matrix can be cached.
 
-## Write a short comment describing this function
-
+#creates a special matrix object which can chache its matrix value
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL ## make inverse value null for freshly created matrix
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)  
 
 }
 
 
-## Write a short comment describing this function
+## calcuate the inverse of a matrix. If the inverse has already been calculated
+## previously, it will return the cached value. Otherwise, a fresh value is computed and returned after being cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getInverse()
+  if (!is.null(inv)) { ## cached value found. return it
+    message("Already computed. Returning cached value.")
+    return(inv)
+  }
+  message("Returning freshly computed value.")  
+  mat <- x$get()
+  inv <- solve(mat, ...)  ## compute fresh inverse
+  
+  x$setInverse(inv)   ## cache the computed inverse
+  inv
+
 }
